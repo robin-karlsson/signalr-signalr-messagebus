0da291f5010292356c1230ef9ed5e63615fa6a06
robink-teleopti <robin.karlsson@teleopti.com>
2012-11-12 13:28:33 +0100

    Creating basic functionality for dictionary scavaging.

--- Contrib.SignalR.SignalRMessageBus/Contrib.SignalR.SignalRMessageBus.Backend/SignalRBackplane.cs
+++ Contrib.SignalR.SignalRMessageBus/Contrib.SignalR.SignalRMessageBus.Backend/SignalRBackplane.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Microsoft.AspNet.SignalR;
 
 namespace Contrib.SignalR.SignalRMessageBus.Backend
@@ -9,6 +11,36 @@ namespace Contrib.SignalR.SignalRMessageBus.Backend
 	{
 		private static readonly object lockobj = new object();
 		private static readonly IDictionary<long,string> messageDictionary = new ConcurrentDictionary<long, string>();
+		private static Timer timer;
+
+		public override void Initialize(IDependencyResolver resolver, HostContext context)
+		{
+			lock (lockobj)
+			{
+				if (timer==null)
+				{
+					timer = new Timer(scavageDictionary,null,TimeSpan.FromSeconds(60),TimeSpan.FromSeconds(60));
+				}
+			}
+			base.Initialize(resolver, context);
+		}
+
+		private static void scavageDictionary(object state)
+		{
+			if (messageDictionary.Count<100000)
+				return;
+
+			lock (lockobj)
+			{
+				var lastIdToSave = getLastId() - 100000;
+				var keysToRemove = messageDictionary.Keys.Where(k => k < lastIdToSave).ToArray();
+
+				foreach (var key in keysToRemove)
+				{
+					messageDictionary.Remove(key);
+				}
+			}
+		}
 
 		protected override System.Threading.Tasks.Task OnReceivedAsync(IRequest request, string connectionId, string data)
 		{
@@ -21,7 +53,7 @@ namespace Contrib.SignalR.SignalRMessageBus.Backend
 			return Connection.Broadcast(id + "#"+data);
 		}
 
-		private long getLastId()
+		private static long getLastId()
 		{
 			if (messageDictionary.Count == 0) return 0L;
 			return messageDictionary.Keys.Max();
