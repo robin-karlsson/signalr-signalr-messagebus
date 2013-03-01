83e6978de383b8635dfdc1a0d167bb6c94525295
robink-teleopti <robin.karlsson@teleopti.com>
2013-03-01 11:05:20 +0100

    Updating to signalr 1.0.1

--- Contrib.SignalR.SignalRMessageBus/Contrib.SignalR.SignalRMessageBus.Backend/Properties/AssemblyInfo.cs
+++ Contrib.SignalR.SignalRMessageBus/Contrib.SignalR.SignalRMessageBus.Backend/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-rc2")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
